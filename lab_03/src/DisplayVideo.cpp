b5fd951804c2ceb7d0322e10fda693de65fddc6e
Andrewchook <andrew@chookaszian.com>
2025-10-14 10:02:31 -0700

    wrap up lab 3

--- lab_03/src/DisplayVideo.cpp
+++ lab_03/src/DisplayVideo.cpp
@@ -1,6 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include <stdio.h>
-#include <string>
+#include <stdlib.h>
 
 using namespace cv;
 
@@ -34,13 +34,57 @@ int main(int argc, char** argv )
 		// Apply grayscale conversion into my program flow using CCIR 601 standard
 		// OpenCV's COLOR_BGR2GRAY uses the coefficients 0.299, 0.587, and 0.114
 		// source: https://docs.opencv.org/3.4/de/d25/imgproc_color_conversions.html#color_convert_rgb_gray
-		Mat gray_frame;
-		cv::cvtColor(frame, gray_frame, cv::COLOR_BGR2GRAY);
+		//cv::cvtColor(frame, gray_frame, cv::COLOR_BGR2GRAY);
+		int cols = frame.cols, rows = frame.rows;
+		Mat gray_frame(rows, cols, CV_8UC1);
+		for (int i=0; i < rows; i++) {
+			// Vec3b represents a single BGR pixel
+			const Vec3b* frame_i = frame.ptr<Vec3b>(i);
+			uint8_t* gray_frame_i = gray_frame.ptr<uint8_t>(i);
+			for (int j=0; j < cols; j++) {
+				uint8_t B = frame_i[j][0];
+				uint8_t G = frame_i[j][1];
+				uint8_t R = frame_i[j][2];
+
+				// Apply grayscale:
+				// Apparently C++ converts the results of multiplications with
+				// floating points to double, hence the typecasting back to uint8_t
+				gray_frame_i[j] = static_cast<uint8_t>(0.114 * B + 0.587 * G + 0.299 * R);
+			}
+		}
+
+		//cv::imshow("Gray Frame", gray_frame);
 
 		// Apply the sobel filter to single out the edges
 		// Use output depth of CV_8U to match the input depth of 8-bit unsigned integers
-		Mat sobel_frame;
-		cv::Sobel(gray_frame, sobel_frame, CV_8U, 1, 1);
+		Mat sobel_frame(rows, cols, CV_8UC1);
+		for (int i=1; i < rows - 1; ++i) {
+			const uint8_t* prevRow = gray_frame.ptr<uint8_t>(i - 1);
+			const uint8_t* currRow = gray_frame.ptr<uint8_t>(i);
+			const uint8_t* nextRow = gray_frame.ptr<uint8_t>(i + 1);
+			uint8_t* sobel_frame_i = sobel_frame.ptr<uint8_t>(i);
+
+			for (int j=1; j < cols - 1; ++j) {
+				// Use unsigned 16-bits in case of overflow past 0-255
+				int16_t Gx = - prevRow[j - 1] + prevRow[j + 1]
+					- 2 * currRow[j - 1] + 2 * currRow[j + 1]
+					- nextRow[j - 1] + nextRow[j + 1];
+
+				int16_t Gy = -prevRow[j - 1] - 2 * prevRow[j] - prevRow[j + 1]
+					+ nextRow[j - 1] + 2 * nextRow[j] + nextRow[j + 1];
+
+				// Use |G| = |Gx| + |Gy| since square and square-root are outrageously
+				// expensive
+				int16_t absG = abs(Gx) + abs(Gy);
+
+				// Clamp high (no negatives due to abs) values
+				// manually to avoid modulo of typecasting
+				if (absG > 255) absG = 255;
+
+				// Convert to uint8
+				sobel_frame_i[j] = static_cast<uint8_t>(absG);
+			}
+		}
 
 		cv::imshow("Sobel Frame", sobel_frame);
 		//cv::waitKey(0);
@@ -58,19 +102,3 @@ int main(int argc, char** argv )
 	return 0;
 }
 
-
-Mat to442_grayscale(Mat input){
-	int height=input.rows;
-	int length=input.cols;
-	Mat output(height,length,CV_8U);
-	for(int i=0;i<height;i++){
-		for(int j=0;j<length;j++){
-			printf("%d, %d\n",i,j);
-		}
-	}	      
-       return output;	
-}
-
-//Mat to443_sobel(Mat){
-
-//}
