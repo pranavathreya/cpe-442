68642c129d3e1848461f3ba019992524a8e81b2e
Andrewchook <andrew@chookaszian.com>
2025-10-14 10:54:31 -0700

    moved to functions and fixed errors

--- tutorial_03/src/DisplayVideo.cpp
+++ tutorial_03/src/DisplayVideo.cpp
@@ -10,12 +10,11 @@ void grayScale(Mat* frame, Mat* gray_frame){
 	// OpenCV's COLOR_BGR2GRAY uses the coefficients 0.299, 0.587, and 0.114
 	// source: https://docs.opencv.org/3.4/de/d25/imgproc_color_conversions.html#color_convert_rgb_gray
 	//cv::cvtColor(frame, gray_frame, cv::COLOR_BGR2GRAY);
-	int cols = frame.cols, rows = frame.rows;
-	for (int i=0; i < rows; i++) {
+	for (int i=0; i < gray_frame->rows; i++) {
 		// Vec3b represents a single BGR pixel
-		const Vec3b* frame_i = frame.ptr<Vec3b>(i);
-		uint8_t* gray_frame_i = gray_frame.ptr<uint8_t>(i);
-		for (int j=0; j < cols; j++) {
+		const Vec3b* frame_i = (*frame).ptr<Vec3b>(i);
+		uint8_t* gray_frame_i = (*gray_frame).ptr<uint8_t>(i);
+		for (int j=0; j < gray_frame->cols; j++) {
 			uint8_t B = frame_i[j][0];
 			uint8_t G = frame_i[j][1];
 			uint8_t R = frame_i[j][2];
@@ -31,13 +30,13 @@ void grayScale(Mat* frame, Mat* gray_frame){
 void sobelFilter(Mat* gray_frame, Mat* sobel_frame){
 // Apply the sobel filter to single out the edges
 	// Use output depth of CV_8U to match the input depth of 8-bit unsigned integers
-	for (int i=1; i < rows - 1; ++i) {
-		const uint8_t* prevRow = gray_frame.ptr<uint8_t>(i - 1);
-		const uint8_t* currRow = gray_frame.ptr<uint8_t>(i);
-		const uint8_t* nextRow = gray_frame.ptr<uint8_t>(i + 1);
-		uint8_t* sobel_frame_i = sobel_frame.ptr<uint8_t>(i);
+	for (int i=1; i < gray_frame->rows - 1; ++i) {
+		const uint8_t* prevRow = gray_frame->ptr<uint8_t>(i - 1);
+		const uint8_t* currRow = gray_frame->ptr<uint8_t>(i);
+		const uint8_t* nextRow = gray_frame->ptr<uint8_t>(i + 1);
+		uint8_t* sobel_frame_i = sobel_frame->ptr<uint8_t>(i);
 
-		for (int j=1; j < cols - 1; ++j) {
+		for (int j=1; j < gray_frame->cols - 1; ++j) {
 			// Use unsigned 16-bits in case of overflow past 0-255
 			int16_t Gx = - prevRow[j - 1] + prevRow[j + 1]
 				- 2 * currRow[j - 1] + 2 * currRow[j + 1]
@@ -89,6 +88,7 @@ int main(int argc, char** argv )
 			break;
 		}
 
+		int cols = frame.cols, rows = frame.rows;
 		Mat gray_frame(rows, cols, CV_8UC1);
 		grayScale(&frame,&gray_frame);
 		//cv::imshow("Gray Frame", gray_frame);
